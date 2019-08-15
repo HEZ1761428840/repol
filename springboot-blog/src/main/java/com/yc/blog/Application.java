e89b749f39dd6a0b155433b8adac5a086123cf6b
HEZ1761428840 <1761428840@qq.com>
2019-08-15 09:11:44 +0800

    博客项目的初始化提交

---
+++ springboot-blog/src/main/java/com/yc/blog/Application.java
@@ -0,0 +1,24 @@
+package com.yc.blog;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+@MapperScan("com.yc")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping("helle")
+	public String hello() {
+		return "hello";
+		
+	}
+
+}
